8323118fc8755f26a07af3407f11f6c179a8823d
DelphineLecorney <lecorney.delphine@gmail.com>
2023-08-04 20:04:55 +0200

    Modify view in routes

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <!-- <script src="/app.js"></script> -->
+    <title>My training laravel</title>
+</head>
+<body>
+    <h1>Hi, I'm training in Laravel</h1>
+
+    <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero in eius voluptatem distinctio rem quo, quibusdam qui tempore ut. Ipsum corrupti voluptates quos molestias numquam facilis officiis praesentium debitis amet.
+    </article>
+</body>
+</html>
